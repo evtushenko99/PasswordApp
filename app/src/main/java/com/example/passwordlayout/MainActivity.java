486780792bc7c75872be6ee1e8067e9ec598256f
maxim <evtushenko99@mail.ru>
2019-11-19 16:41:00 +0300

    Initial commit

---
+++ app/src/main/java/com/example/passwordlayout/MainActivity.java
@@ -0,0 +1,191 @@
+package com.example.passwordlayout;
+
+import android.app.Activity;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends Activity {
+    private final static int MIN_LENGTH = 8;
+    private final static String PASSWORD_LENGTH = "Длина: " + MIN_LENGTH;
+
+    private TextView mResultTextView;
+    private TextView mQualityText;
+    private TextView mSeekBarHelperTextView;
+    private TextView mGeneratedPasswordText;
+
+    private EditText mSourceTextView;
+    private PasswordsHelper mHelper;
+    private View mCopyButton;
+    private View mCopyGeneratedPasswordButton;
+
+    private CompoundButton mCheckBoxUpperCase;
+    private CompoundButton mCheckBoxNumbers;
+    private CompoundButton mCheckBoxSpecialCharacters;
+    private ArrayList<Integer> mAdditions;
+    private Button mGeneratePasswordButton;
+    private SeekBar mSeekBar;
+
+    private ImageView mQuality;
+    private int mLength;
+    private StringBuilder mTextSeekBarString;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextSeekBarString = new StringBuilder();
+        mTextSeekBarString.append(PASSWORD_LENGTH);
+
+        mAdditions = new ArrayList<>();
+        mAdditions.add(0);
+
+        mHelper = new PasswordsHelper(
+                getResources().getStringArray(R.array.russian),
+                getResources().getStringArray(R.array.english)
+        );
+
+
+        mResultTextView = findViewById(R.id.result_text);
+        mSourceTextView = findViewById(R.id.source_text);
+        mSeekBarHelperTextView = findViewById(R.id.text_seekBar_helper);
+        mGeneratedPasswordText = findViewById(R.id.result_text_generated_password);
+
+        mCopyButton = findViewById(R.id.button_copy);
+        mCopyGeneratedPasswordButton = findViewById(R.id.button_copy_generated_password);
+        mGeneratePasswordButton = findViewById(R.id.button_generated_password);
+
+        mQuality = findViewById(R.id.quality);
+        mQualityText = findViewById(R.id.quality_text);
+
+        mCheckBoxUpperCase = findViewById(R.id.check_uppercase);
+        mCheckBoxNumbers = findViewById(R.id.check_numbers);
+        mCheckBoxSpecialCharacters = findViewById(R.id.check_special_characters);
+        mSeekBar = findViewById(R.id.seekBar);
+
+        mSeekBarHelperTextView.setText(PASSWORD_LENGTH);
+
+        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                mTextSeekBarString.delete(PASSWORD_LENGTH.length(), mTextSeekBarString.length());
+                if (i > 0) {
+                    mTextSeekBarString.append(" + ");
+                    mTextSeekBarString.append(getResources().getQuantityString(R.plurals.symbols_plurals, i, i));
+                    mTextSeekBarString.append(" = ");
+                    mTextSeekBarString.append(getResources().getQuantityString(R.plurals.symbols_plurals, MIN_LENGTH + i, MIN_LENGTH + i));
+                }
+
+                mSeekBarHelperTextView.setText(mTextSeekBarString.toString());
+                mLength = MIN_LENGTH;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                mLength += mSeekBar.getProgress();
+            }
+        });
+
+        mCopyButton.setEnabled(false);
+        mCopyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ClipboardManager manager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                assert manager != null;
+                manager.setPrimaryClip(ClipData.newPlainText(
+                        MainActivity.this.getString(R.string.clipboard_title),
+                        mResultTextView.getText()
+                ));
+                Toast.makeText(MainActivity.this, "Скопировано в буффер обмена", Toast.LENGTH_SHORT).show();
+            }
+        });
+        mCopyGeneratedPasswordButton.setEnabled(false);
+        mCopyGeneratedPasswordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ClipboardManager manager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                assert manager != null;
+                manager.setPrimaryClip(ClipData.newPlainText(
+                        MainActivity.this.getString(R.string.clipboard_title),
+                        mGeneratedPasswordText.getText()
+                ));
+                Toast.makeText(MainActivity.this, "Скопировано в буффер обмена", Toast.LENGTH_SHORT).show();
+            }
+        });
+        mSourceTextView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                mResultTextView.setText(mHelper.convert(charSequence));
+                mCopyButton.setEnabled(charSequence.length() > 0);
+                int mGetQuality = mHelper.getQuality(charSequence);
+                mQuality.setImageLevel((mGetQuality * 1000));
+                mQualityText.setText(getResources().getStringArray(R.array.qualities)[mGetQuality]);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        mCheckBoxUpperCase.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                mAdditions.add(1);
+            }
+        });
+        mCheckBoxNumbers.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                mAdditions.add(2);
+            }
+        });
+        mCheckBoxSpecialCharacters.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                mAdditions.add(3);
+            }
+
+        });
+        mGeneratePasswordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!mCheckBoxNumbers.isChecked() && mAdditions.size() > 0) {
+                    mAdditions.remove((Integer) 2);
+                }
+                if (!mCheckBoxSpecialCharacters.isChecked() && mAdditions.size() > 0) {
+                    mAdditions.remove((Integer) 3);
+                }
+                if (!mCheckBoxUpperCase.isChecked() && mAdditions.size() > 0) {
+                    mAdditions.remove((Integer) 1);
+                }
+
+                mGeneratedPasswordText.setText(mHelper.passwordGenerate(mLength, mAdditions));
+                mCopyGeneratedPasswordButton.setEnabled(mGeneratedPasswordText.length() > 0);
+            }
+        });
+
+    }
+
+}
---
+++ app/src/main/java/com/example/passwordlayout/PasswordsHelper.java
@@ -0,0 +1,76 @@
+package com.example.passwordlayout;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class PasswordsHelper {
+    private String[] mRussians;
+    private String[] mLatins;
+    private static final String SpecialCharacters = "!@#$%&*()_+-=[]|,./?><";
+
+    public PasswordsHelper(String[] russians, String[] latins) {
+        if (russians.length != latins.length) {
+            throw new IllegalArgumentException();
+        }
+        mRussians = russians;
+        mLatins = latins;
+    }
+
+    public String convert(CharSequence source) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < source.length(); i++) {
+            char c = source.charAt(i);
+            String key = String.valueOf(Character.toLowerCase(c));
+
+            for (int dict = 0; dict < mRussians.length; dict++) {
+                if (key.equals(mRussians[dict])) {
+                    result.append(Character.isUpperCase(c) ? mLatins[dict].toUpperCase() : mLatins[dict]);
+                }
+            }
+            if (result.length() <= i) {
+                result.append(c);
+            }
+
+        }
+        return result.toString();
+    }
+
+    public int getQuality(CharSequence password) {
+        return Math.min(password.length(), 10);
+    }
+
+    public String passwordGenerate(int length, ArrayList additions) {
+        StringBuilder pass = new StringBuilder(length);
+        Random random = new Random();
+        pass.append((char) (random.nextInt(26) + 'a'));
+        for (int i = 1; i < length; ++i) {
+            char next = '0';
+            int range = 10;
+            switch ((int) additions.get(random.nextInt(additions.size()))) {
+                case 0: {
+                    next = 'a';
+                    range = 26;
+                }
+                break;
+                case 1: {
+                    next = 'A';
+                    range = 26;
+                }
+                break;
+                case 2: {
+                    next = '0';
+                    range = 10;
+                }
+                break;
+                case 3: {
+                    next = '!';
+                    range = 15;
+                }
+                break;
+            }
+            pass.append((char) (random.nextInt(range) + next));
+        }
+
+        return pass.toString();
+    }
+}
